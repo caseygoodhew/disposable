917b2ac0bf16a74b6db846a4045e38280749db7c
caseygoodhew <goodhewc@gmail.com>
2014-08-13 07:31:47 +0100

    ConverterFactory improvements for Many

---
+++ code/Disposable.Common/Conversion/ConvertToSelf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposable.Common.Conversion
+{
+    internal class ConvertToSelf<T> : IConvert<T, T> where T : class
+    {
+        public T Convert(T fromType)
+        {
+            return fromType;
+        }
+    }
+}
--- code/Disposable.Common/Conversion/ConverterFactory.cs
+++ code/Disposable.Common/Conversion/ConverterFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using Disposable.Common.ServiceLocator;
 
@@ -6,9 +8,20 @@ namespace Disposable.Common.Conversion
 {
     public static class ConverterFactory
     {
-        public static TTo Convert<TFrom, TTo>(TFrom from) where TFrom : class where TTo : class
+        public static TTo ConvertOne<TFrom, TTo>(TFrom fromType)
+            where TFrom : class
+            where TTo : class
         {
-            return Locator.Current.Instance<IConvert<TFrom, TTo>>().Convert(from);
+            return Locator.Current.Instance<IConvert<TFrom, TTo>>().Convert(fromType);
+        }
+
+        public static IEnumerable<TTo> ConvertMany<TFrom, TTo>(IEnumerable<TFrom> fromType)
+            where TFrom : class
+            where TTo : class
+        {
+            var converter = Locator.Current.Instance<IConvert<TFrom, TTo>>();
+
+            return fromType.Select(converter.Convert);
         }
 
         public static void Register<TFrom, TTo>(IRegistrar registrar, Func<TFrom, TTo> func)
@@ -16,6 +29,11 @@ namespace Disposable.Common.Conversion
             where TTo : class
         {
             registrar.Register<IConvert<TFrom, TTo>>(() => new FunctionalConverter<TFrom, TTo>(func));
+        }
+
+        public static void Register<T>(IRegistrar registrar) where T : class
+        {
+            registrar.Register<IConvert<T, T>>(() => new ConvertToSelf<T>());
         }
     }
 }
