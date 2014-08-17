f5a4dff7e70abdfda1c69e8a9446790cd7d243d8
caseygoodhew <goodhewc@gmail.com>
2014-08-17 08:36:41 +0100

    GuardedValue

---
+++ code/Disposable.Common/Extensions/GuardedValueExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Disposable.Common.Extensions
+{
+    public static class GuardedValueExtensions
+    {
+        private static T GuardedValue<T>(this T? value, string argumentName = "value") where T : struct
+        {
+            Guard.ArgumentNotNull(value, argumentName);
+            return value.Value;
+        }
+    }
+}
