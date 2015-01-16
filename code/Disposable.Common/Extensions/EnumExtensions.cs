79a83b8cfbaede72f5f185d91c6e2f29de4daa4d
caseygoodhew <goodhewc@gmail.com>
2015-01-16 19:03:00 +0000

    enum extensions

--- code/Disposable.Common/Extensions/EnumExtensions.cs
+++ code/Disposable.Common/Extensions/EnumExtensions.cs
@@ -1,14 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Disposable.Common.Extensions
 {
     public static class EnumExtensions
     {
+        public static bool IsEnum<T>()
+        {
+            return typeof(T).IsEnum;
+        }
+
+        public static void Guard<T>()
+        {
+            if (!IsEnum<T>())
+            {
+                throw new InvalidEnumArgumentException(string.Format("{0} is not an enum", typeof(T).FullName));
+            }
+        }
+        
         public static IEnumerable<T> GetValues<T>()
         {
-            return Enum.GetValues(typeof(T)).Cast<T>();
+            return All<T>();
+        }
+
+        public static IEnumerable<T> All<T>()
+        {
+            Guard<T>();
+            return Enum.GetValues(typeof(T)).Cast<T>(); 
+        }
+
+        public static IEnumerable<T> Except<T>(params T[] values)
+        {
+            return All<T>().Where(x => !values.Contains(x));
         }
     }
 }
